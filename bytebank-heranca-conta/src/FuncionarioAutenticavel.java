0d8d9d2567eba25d2fa67c149bd23626a53cc051
abrazao <anderson.brazao@gmail.com>
2022-02-28 17:00:41 -0300

    Não existe herança múltipla em Java. Conceitos de interface. Diferenças entre classes abstratas e interfaces. interfaces são uma alternativa a herança referente ao polimorfismo

--- bytebank-heranca-conta/src/FuncionarioAutenticavel.java
+++ bytebank-heranca-conta/src/FuncionarioAutenticavel.java
@@ -1,4 +1,17 @@
 
-public class FuncionarioAutenticavel {
+public abstract class FuncionarioAutenticavel extends Funcionario{
 
+    private int senha;
+
+    public void setSenha(int Senha){
+        this.senha = senha;
+    }
+
+    public boolean autentica(int senha){
+        if(this.senha == senha){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
